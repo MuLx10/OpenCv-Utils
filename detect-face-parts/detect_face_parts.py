b532db2b8bb41cf31f10143dd42f2f6390d47470
Mehul Kumar Nirala <MuLx10@users.noreply.github.com>
2017-09-15 14:03:54 +0530

    Update detect_face_parts.py

--- detect-face-parts/detect_face_parts.py
+++ detect-face-parts/detect_face_parts.py
@@ -1,5 +1,5 @@
 # USAGE
-# python detect_face_parts.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_01.jpg 
+# python detect_face_parts.py --shape-predictor face_landmarks.dat --image images/example_01.jpg 
 
 # import the necessary packages
 from imutils import face_utils
